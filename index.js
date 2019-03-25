a9b56c213bf08d1f458b33a2f6263ee2b9f25e09
Zachary Lehmann <zlehmann@gmail.com>
2019-03-25 20:08:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function produceDrivingRange(range) {
-  console.log(range);
   return function(pos1, pos2) {
-    console.log(pos1);
-    console.log(pos2);
     let block1 = pos1.splice(2,2);
     let block2 = pos2.splice(2,2);
-    console.log(block1);
-    console.log(block2);
-    return Math.abs(pos1-pos2);
+    let distance = pos2-pos1;
+    let result = 0;
+    if (distance <= range) {
+      result = ('within range by ' + (distance - range));
+    } else {
+      result = ((distance - range) + 'blocks out of range');
+    }
   };
 }
