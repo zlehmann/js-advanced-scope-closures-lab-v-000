7ea0378c4be6b7dcee07095a2b2d706692e23474
Zachary Lehmann <zlehmann@gmail.com>
2019-03-25 20:23:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,14 @@ function produceTipCalculator(percent) {
     return fare * percent;
   };
 }
+
+function createDriver() {
+  let driverId = 0;
+
+  return class {
+    constructor(name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  };
+}
