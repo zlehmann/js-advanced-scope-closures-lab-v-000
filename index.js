10db756344f2eaecefbfbae1c6d6c7b8d3f1ae3c
Zachary Lehmann <zlehmann@gmail.com>
2019-03-25 20:03:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function produceDrivingRange(range) {
+  console.log(range);
   return function(pos1, pos2) {
+    console.log(pos1);
+    console.log(pos2);
     let block1 = pos1.splice(2,2);
     let block2 = pos2.splice(2,2);
+    console.log(block1);
+    console.log(block2);
     return Math.abs(pos1-pos2);
   };
 }
