08a7d030da27d46843433afdaad7a3839ce4c546
Zachary Lehmann <zlehmann@gmail.com>
2019-03-25 20:16:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function produceDrivingRange(range) {
     console.log('inside inner function');
     let block1 = pos1.slice(0,-2);
     let block2 = pos2.slice(0,-2);
-    console.log(block1);
+    console.log('block1 is ' + block1);
     let distance = block2-block1;
     let result = 0;
     console.log(distance);
