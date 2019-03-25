dde22425c07da0719d05942f3e510552bf1b2048
Zachary Lehmann <zlehmann@gmail.com>
2019-03-25 20:17:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function produceDrivingRange(range) {
-  console.log('is this working at all');
   return function(pos1, pos2) {
-    console.log('inside inner function');
     let block1 = pos1.slice(0,-2);
     let block2 = pos2.slice(0,-2);
     console.log('block1 is ' + block1);
     let distance = block2-block1;
     let result = 0;
-    console.log(distance);
+    console.log('distance is ' + distance);
     if (distance <= range) {
       result = ('within range by ' + (distance - range));
     } else {
